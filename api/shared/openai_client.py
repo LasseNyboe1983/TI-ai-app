0817cae63a554de9afc7912f9196958e2eba4183
LasseNyboe1983 <lasn@teknologisk.dk>
2026-02-18 09:14:24 +0100

    Wipe repository to clean baseline

--- api/shared/openai_client.py
+++
@@ -1,9 +0,0 @@
-import os
-from openai import AzureOpenAI
-
-def get_client() -> AzureOpenAI:
-  return AzureOpenAI(
-    api_key=os.environ["AZURE_OPENAI_KEY"],
-    api_version="2025-03-01-preview",
-    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
-  )
